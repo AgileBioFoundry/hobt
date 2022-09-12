2d8a3fa7cbd84a6ce3f8843d61058795507b26aa
Hector Plahar <hector.plahar@gmail.com>
2022-09-12 09:37:38 -0700

    cache number of available protocols

--- src/main/java/org/abf/hobt/service/protocols/Protocols.java
+++ src/main/java/org/abf/hobt/service/protocols/Protocols.java
@@ -1,5 +1,7 @@
 package org.abf.hobt.service.protocols;
 
+import org.abf.hobt.cache.ElementCacheType;
+import org.abf.hobt.cache.ElementCaches;
 import org.abf.hobt.common.ResultData;
 import org.abf.hobt.dao.DAOFactory;
 import org.abf.hobt.dao.model.OrganismModel;
@@ -10,27 +12,27 @@ import java.util.Map;
 
 public class Protocols {
 
-    private static final String FILTER = "filter";
-    private static final String SEARCH_KEY = "key";
+    private static final String FILTER = "filter";          // protocols filter (currently retrieving only 'public')
+    private static final String SEARCH_KEY = "key";         // search term for searching protocol names, desc, authors
     private static final String ORDER_FIELD = "order_field"; // activity (default), relevance, date, name, id
-    private static final String ORDER_DIR = "order_dir";
-    private static final String FIELDS = "fields";  // list of fields from "item". comma separated
-    private static final String PAGE_SIZE = "page_size"; // number of items per one page 1...100. default is 10
-    private static final String PAGE_ID = "page_id"; // id of page 1...n (default is 1)
+    private static final String ORDER_DIR = "order_dir";     // asc or desc (default)
+    private static final String FIELDS = "fields";           // list of fields from "item". comma separated
+    private static final String PAGE_SIZE = "page_size";    // number of items per one page 1...100. default is 10
+    private static final String PAGE_ID = "page_id";        // id of page 1...n (default is 1)
 
     public Protocols() {
     }
 
     /**
      * Retrieves (paged) protocols (currently from protocols.io) for the specified organism. Retrieval is done using
-     * the name of the organism as the search term
+     * the name of the organism as the search term. The number available is cached
      *
-     * @param organismId
-     * @param start
-     * @param limit
-     * @param sort
-     * @param asc
-     * @return
+     * @param organismId unique identifier for host organism
+     * @param start      start offset
+     * @param limit      max results to return
+     * @param sort       sort field
+     * @param asc        sort order
+     * @return wrapper around list of retrieved protocols and available count
      */
     public ResultData<Protocol> getByOrganism(long organismId, int start, int limit, String sort, boolean asc) {
         // retrieve
@@ -46,6 +48,10 @@ public class Protocols {
         callParameters.put(PAGE_SIZE, Integer.toString(limit));
         callParameters.put(PAGE_ID, Integer.toString(start + 1));
 
-        return ProtocolsIORestClient.getInstance().get(callParameters);
+        ResultData<Protocol> resultData = ProtocolsIORestClient.getInstance().get(callParameters);
+
+        // update cache
+        new ElementCaches(organismId).updateStatistics(ElementCacheType.PROTOCOL, resultData.getAvailable());
+        return resultData;
     }
 }
