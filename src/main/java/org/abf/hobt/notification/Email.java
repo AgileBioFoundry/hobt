89e21840861e4b69cd4099254a0ac0f50e07ff3a
Hector Plahar <hector.plahar@gmail.com>
2024-12-20 08:11:03 -0800

    remove javax.mail and use mailer

--- src/main/java/org/abf/hobt/notification/Email.java
+++ src/main/java/org/abf/hobt/notification/Email.java
@@ -4,16 +4,10 @@ import org.abf.hobt.common.logging.Logger;
 import org.abf.hobt.common.util.StringUtils;
 import org.abf.hobt.config.ConfigurationValue;
 import org.abf.hobt.config.Settings;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.util.Properties;
+import org.simplejavamail.api.email.EmailPopulatingBuilder;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
 
 /**
  * @author Hector Plahar
@@ -28,33 +22,44 @@ public class Email {
 
     public void send(String email, String ccEmail, String subject, String body) {
         Logger.info("Sending email to " + email + ": cced " + ccEmail + " with subject " + subject);
+
         String smtpHost = new Settings().getValue(ConfigurationValue.SMTP_HOST);
         if (StringUtils.isBlank(smtpHost)) {
-            Logger.error("Smtp host has not been set. Aborting email send.");
+            Logger.error("SMTP host is not configured. Aborting email send.");
             return;
         }
 
-        Properties props = new Properties();
-        props.put("mail.smtp.host", smtpHost);
-        Session session = Session.getDefaultInstance(props, null);
-
-        Message msg = new MimeMessage(session);
-
         String senderEmail = new Settings().getValue(ConfigurationValue.FROM_EMAIL);
 
+        // Configure the mailer
+        Mailer mailer = MailerBuilder
+            .withSMTPServer(smtpHost, 25) // Change the port if required
+            // Uncomment and set if authentication is required:
+            // .withSMTPServerUsername("username")
+            // .withSMTPServerPassword("password")
+            .buildMailer();
+
         try {
-            msg.setFrom(new InternetAddress(senderEmail, projectName));
-            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(email, projectName));
-            if (!StringUtils.isBlank(ccEmail))
-                msg.addRecipient(Message.RecipientType.CC, new InternetAddress(ccEmail));
-//            if (!StringUtil.isEmpty(adminEmail))
-//                msg.addRecipient(Message.RecipientType.BCC, new InternetAddress(adminEmail));
-            msg.setSubject(subject);
-            msg.setSentDate(new Date());
-            msg.setText(body);
-            Transport.send(msg);
-        } catch (MessagingException | UnsupportedEncodingException e) {
-            Logger.error(e);
+            // Build the email
+            EmailPopulatingBuilder builder = EmailBuilder.startingBlank()
+                .from(projectName, senderEmail)
+                .to(projectName, email)
+                .withSubject(subject)
+                .withPlainText(body);
+
+            // Add CC if provided
+            if (!StringUtils.isBlank(ccEmail)) {
+                builder.cc("", ccEmail);
+            }
+
+            // Build the final email
+            org.simplejavamail.api.email.Email emailMessage = builder.buildEmail();
+
+            // Send the email
+            mailer.sendMail(emailMessage);
+            Logger.info("Email sent successfully.");
+        } catch (Exception e) {
+            Logger.error("Error sending email", e);
         }
     }
 }
